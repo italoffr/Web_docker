31cbffa90328de5d5734e95a101eaeaad0669691
Italo Gomes <italogsilvas10@gmail.com>
2022-09-18 13:51:35 -0300

    aluno nova porta

--- pyaluno.py
+++ pyaluno.py
@@ -37,5 +37,5 @@ def lista():
      return render_template('infolista.html', datas=data)
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5008))
+    port = int(os.environ.get("PORT", 5003))
     app.run(host='0.0.0.0', port=port)
